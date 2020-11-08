7646db2e9b64e9cd8b49f98bfff978fe0acf1ca0
chong <44306843+GabrielRalph@users.noreply.github.com>
2020-11-08 18:00:19 +1100

    Transition to firestore
    Firestore more work than needed

--- cshare/DoxyFire.js
+++ cshare/DoxyFire.js
@@ -13,30 +13,81 @@ firebase.initializeApp(firebaseConfig);
 
 class DoxyFire extends SvgPlus{
   build(){
-    this.getFiles();
     this.props = {class: 'doxy-fire'}
+    this.doxygens = {}
+    this.cshare = firebase.firestore().collection('cshare');
   }
-  async getFiles(){
-    this.innerHTML = '';
-    let sn = await firebase.database().ref('cshare/').once('value');
-    let files = sn.val();
-    for (var name in files){
-      let revs = files[name];
-      name = name.replace('_', '.');
 
-      let rev_name = Object.keys(revs)
-      console.log(rev_name);
+  set roomName(val){
+    if (typeof val !== 'string') return;
+    this._roomName = val;
+  }
+  get roomName(){
+    return this._roomName;
+  }
 
-      rev_name = rev_name[rev_name.length - 1];
-      console.log(rev_name);
 
-      let doxygen = new Doxygen('div');
-      doxygen.code = revs[rev_name]
-      doxygen.header = name;
-      this.appendChild(doxygen)
-      console.log(name);
+  get collection(){
+    if (typeof this.roomName !== 'string') return null;
+    return this.cshare.doc(this.roomName).collection(this.roomName);
+  }
+
+  isValidFile(file){
+    if (!(file instanceof File)) return false;
+
+    let fileName = file.name;
+    return this.isValidFileName(fileName)
+  }
+
+  isValidFileName(fileName){
+    if (typeof fileName !== 'string') return false;
+    if ((/\.c$/).test(fileName)){
+      return true;
+    }else{
+      throw 'The file provided was not a c file'
     }
   }
+
+  makeDoxygen(code, name){
+    if (!this.doxygens[name]){
+      this.doxygens[name] = new Doxygen('div');
+      this.appendChild(this.doxygens[name]);
+    }
+
+    let doxygen = this.doxygens[name];
+
+    doxygen.code = code;
+    doxygen.header = name;
+  }
+
+  async watchFiles(){
+    let fileDocs;
+    if (this.collection == null) return false;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => { reject(false) }, 10000);
+
+      try{
+
+        this.collection.where('current', '==', true).onSnapshot((fileDocs) => {
+          console.log('snapshot');
+
+          fileDocs.docChanges().forEach((change) => {
+            let rev = change.doc.data();
+            this.makeDoxygen(rev.code, rev.name);
+          })
+
+          resolve(true);
+        }, () => {
+          resolve(false);
+        })
+
+      }catch(e){
+        console.log(e);
+        resolve(false);
+      }
+    })
+  }
+
 
   async readFile(file){
     return new Promise((resolve, reject) => {
@@ -50,24 +101,99 @@ class DoxyFire extends SvgPlus{
     })
   }
 
+
   async addFiles(fileList){
     if (!(fileList instanceof FileList)) return;
     for (var i = 0; i < fileList.length; i++){
-      console.log(fileList[i]);
-      let res = await this.addFile(fileList[i]);
-      if (res == null) console.log(fileList[i].filename + ' was not uploaded');
+      try{
+        let res = await this.addFile(fileList[i]);
+      }catch(e){
+        alert(`Error loading files\n${e}`)
+      }
     }
+    return true;
+  }
+
+
+  async addRevision(newFile){
+
+    //Try and get the last revision of the file
+    var oldFile;
+    try{
+      let oldFileDocs = await this.collection.where('name', '==', newFile.name).orderBy('rev', 'desc').limit(1).get();
+      oldFile = oldFileDocs.docs[0].data();
+
+    //If no revision could be found
+    }catch(e){
+      console.log('newest revision not found');
+      return false;
+    }
+
+    //This shouldn't happen unless db has been tampered with
+    if (!oldFile.current) console.log('Newest revision is not set as current');
+    console.log(oldFile);
+
+    //Try and set the oldFile to not current
+    try{
+      await this.collection.doc(oldFile.name + oldFile.rev).update({
+        current: false
+      })
+
+    //Fails to set current of the oldFile false
+    }catch(e){
+      console.log('failed to change the old revision from current to not current');
+      console.log(e);
+      return false;
+    }
+
+    //Try and set the new file doc
+    newFile.rev = oldFile.rev + 1;
+    try{
+      let res = await this.collection.doc(newFile.name + newFile.rev).set(newFile);
+
+    //File was not set
+    }catch(e){
+      console.log('Failed to add new revision');
+
+      //Reset old revision to new current
+      try{
+        await this.collection.doc(oldFile.name + oldFile.rev).update({
+          current: true
+        })
+
+      //Reset did not occurr
+      }catch(e2){
+        console.log('Failed to set the old revision back to current');
+        return false;
+      }
+    }
+
+    console.log('Success!');
+    return true;
   }
 
   async addFile(file){
-    if (!(file instanceof File)) return null;
+    if (!this.isValidFile(file)) return false;
 
-    let text = await this.readFile(file);
-    if (text == null) return null;
-    console.log(text);
+    let newFileCode = await this.readFile(file);
 
-    var filename = file.name.replace('.', '_');
-    let pushkey = await firebase.database().ref('/cshare/'+filename).push();
-    return pushkey.set(text);
+    let newFile = {
+      rev: 0,
+      name: file.name,
+      code: newFileCode,
+      current: true,
+    }
+
+    try{
+      console.log('adding file ' + newFile.name);
+      await this.collection.doc(newFile.name + newFile.rev).set(newFile);
+
+    }catch(e){
+      console.log('createing new revision');
+      return await this.addRevision(newFile);
+    }
+
+    console.log('Succussfully added new file');
+    return true;
   }
 }
